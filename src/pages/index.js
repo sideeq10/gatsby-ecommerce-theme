b3994c124a55e5537569816f1ff0c7b345ee6c08
sideeq10 <109958969+sideeq10@users.noreply.github.com>
2023-07-09 09:12:45 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -98,7 +98,7 @@ const IndexPage = () => {
       {/* Quote */}
       <Quote
         bgColor={'var(--standard-light-grey)'}
-        title={'about Sydney'}
+        title={'about Sideeq young'}
         quote={
           '“We believe in two things: the pursuit of quality in everything we do, and looking after one another. Everything else should take care of itself.”'
         }
