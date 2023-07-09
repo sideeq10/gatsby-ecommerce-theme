a6674b1854de1069c4c242450b85c5d1ca4ecbb0
sideeq10 <109958969+sideeq10@users.noreply.github.com>
2023-07-09 09:41:35 +0100

    Update Brand.js

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -10,7 +10,7 @@ const Brand = (props) => {
       role={'presentation'}
       onClick={() => navigate('/')}
     >
-      {/* <h4>SYDNEY</h4> */}
+      {/* <h4>SIDEEQ YOUNG</h4> */}
       <svg
         width={127}
         height={24}
